36b7a2e153c042615826c94538889fd438988e6b
Codename Anroid <codenamegit@gmail.com>
2012-10-09 10:08:21 -0500

    Revert "revert surfaceflinger: Enable dithering if persist.sys.use_dithering=1|2"
    This reverts commit deb4910770510ff764109f7008eb3ba7428e922a.

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -225,6 +225,9 @@ public:
 
     GLuint getProtectedTexName() const { return mProtectedTexName; }
 
+    // 0: surface doesn't need dithering, 1: use if necessary, 2: use permanently
+    inline int  getUseDithering() const { return mUseDithering; }
+
 
     class MessageDestroyGLTexture : public MessageBase {
         GLuint texture;
@@ -432,6 +435,7 @@ private:
 
    // only written in the main thread, only read in other threads
    volatile     int32_t                     mSecureFrameBuffer;
+                int                         mUseDithering;
 #if defined(BOARD_USES_SAMSUNG_HDMI) && defined(SAMSUNG_EXYNOS5250)
     SecHdmiClient *                         mHdmiClient;
 #endif
